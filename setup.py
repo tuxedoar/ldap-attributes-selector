8e0269dd6920e86eb93993b6c8ab14442b070a05
tuxedoar <tuxedoar@gmail.com>
2019-12-10 19:29:47 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='ldap-attributes-selector',
-    version='0.2.4',
+    version='0.2.5',
     description='Get a CSV formatted list, based on a custom set of LDAP attributes.',
     long_description=long_description,
     long_description_content_type="text/markdown",
